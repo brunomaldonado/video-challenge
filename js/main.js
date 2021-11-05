382fe2dd71b3774b44933eae5f970c9f6e7b277f
brunomaldonado <brunoutj@gmail.com>
2021-11-04 20:23:09 -0600

    Interactive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-//  alert("hello wolrd!!!!!");
-
 const images = document.querySelectorAll("img.carousel-item__img");
 const modal = document.querySelector(".modal");
 const modalimage = document.querySelector(".modal__image");
-// const modaltext = document.querySelector(".modal__text");
 const modalclose = document.querySelector(".close");
 
 images.forEach((image) => {
